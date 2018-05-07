700039e91ffd233c83650b46867ee736be434bf0
zakaria-ben <zakaria.benomar06@gmail.com>
2018-05-07 17:32:39 +0000

    fixing Network-Manager

--- iotronic_lightningrod/modules/network_manager.py
+++ iotronic_lightningrod/modules/network_manager.py
@@ -15,24 +15,28 @@
 
 import asyncio
 from iotronic_lightningrod.modules import Module
-from iotronic_lightningrod.modules import utils
+from urllib.parse import urlparse
 
 import iotronic_lightningrod.wampmessage as WM
 
-from oslo_config import cfg
 from oslo_log import log as logging
 import subprocess
 import random
 import time
+
 LOG = logging.getLogger(__name__)
 
 Port = []
+
+interface = ""
 
 class NetworkManager(Module.Module):
 
     def __init__(self, board, session):
 
         super(NetworkManager, self).__init__("NetworkManager", board)
+        self.url_ip = urlparse(board.wamp_config["url"])[1].split(':')[0]
+        self.wagent_url = "ws://" + self.url_ip + ":8080"
 
     def finalize(self):
         pass
@@ -54,8 +58,11 @@ class NetworkManager(Module.Module):
         return c
 
     async def Create_VIF(self, r_tcp_port):
+
+        LOG.info("Creation of the VIF ")
+
         port_socat = random.randint(10000, 20000)
-        LOG.info(str(port_socat))
+
         i = 0
         while i < len(Port):
             if Port[i] == port_socat:
@@ -63,22 +70,59 @@ class NetworkManager(Module.Module):
                 port_socat = random.randint(10000, 20000)
             i += 1
         Port.insert(0, port_socat)
-        LOG.info(str(port_socat))
 
-        LOG.debug('Creating virtual interface on the board')
+        LOG.debug("Creation of the VIF iotronic"+str(port_socat))
+
+        #LOG.debug('Creating virtual interface on the board')
+
         try:
+            p2 = subprocess.Popen('socat -d -d TCP-L:' + str(port_socat) + ',bind=localhost,reuseaddr,forever,interval=10 TUN,tun-type=tap,tun-name=iotronic' + str(port_socat) + ',up  '
+                                  , shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
-            p2 = subprocess.Popen(
-                'socat -d -d TCP-L:' + str(port_socat) + ',bind=localhost,reuseaddr,forever,interval=10 TUN,tun-type=tap,tun-name=iot3,up &'
-                    , shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-
-            time.sleep(7)
-
+#ws://192.168.17.105:8080
             p1 = subprocess.Popen(
-                'wstun -r' + str(r_tcp_port) + ':localhost:' + str(port_socat) + ' ws://192.168.17.105:8080 &'
+                'wstun -r' + str(r_tcp_port) + ':localhost:' + str(port_socat) + ' '+str(self.wagent_url)
                 , shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-            #return 1
-        except:
-            LOG.error('Error while creating the virtual interface')
-            return 0
 
+            LOG.debug('Creation of the VIF succeded: iotronic')
+
+            global interface
+            interface = 'iotronic'+str(port_socat)
+            message = 'creation de WS tun et SOCAT'
+            w_msg = WM.WampSuccess(message)
+
+        except:
+
+            LOG.error('Error while creating the virtual interface')
+            message = 'Error while the creation'
+            w_msg = WM.WampError(message)
+
+        return w_msg.serialize()
+
+    async def Configure_VIF(self, port_mac):
+
+        LOG.info("Configuration of the VIF")
+
+        try:
+            LOG.debug("Configuration of the VIF "+interface)
+
+            time.sleep(2)
+            p3 = subprocess.Popen("ip link set dev " + str(interface) + " address " + str(port_mac)
+                                  , shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+            time.sleep(4)
+
+            p4 = subprocess.Popen("dhclient " +str(interface), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+            message = 'IP address assigned'
+            w_msg = WM.WampSuccess(message)
+
+            LOG.info("Configuration succeded")
+
+        except Exception as e:
+
+            LOG.error(str(e))
+            message = 'Error while the configuration'
+            w_msg = WM.WampError(message)
+
+        return w_msg.serialize()
